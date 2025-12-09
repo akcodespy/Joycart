42d032facb89ee1ab92b0325b0aae14fc55d507a
akshay-moolath <akmoolath@gmail.com>
2025-12-09 16:45:15 +0530

    mounted static files and made endpoint functions in main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,23 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from app.db import Base, engine
 
+Base.metadata.create_all(bind = engine)
+
+app = FastAPI()
+app.mount('/static',  StaticFiles(directory='static'), name = 'static')
+
+@app.get('/')
+def home():
+    return FileResponse("static/homepage.html")
+@app.get('/login')
+def login():
+    return FileResponse("static/login.html")
+@app.get("/register")
+def register():
+    return FileResponse("static/register.html")
+@app.get("/dashboard")
+def dashboard():
+    return FileResponse("static/dashboard.html")
 
