3553807027af6fdd53c20d74cb29f767dff20a60
akshay-moolath <akmoolath@gmail.com>
2025-12-27 09:07:57 +0530

    fixed orderitems updation

--- app/checkout.py
+++ app/checkout.py
@@ -251,18 +251,54 @@ def place_cod_order(
         raise HTTPException(404)
 
     cart = db.query(Cart).filter(
-        User.id == current_user.id).first()
+        Cart.id == current_user.id).first()
     
+    total_amount = 0
+    order_items: list[OrderItems] = []
+    
+    for item in cart.items:
+        product = (
+            db.query(Product)
+            .filter(Product.id == item.product_id)
+            .first()
+        )
+
+        if not product:
+            continue
+
+        subtotal = product.price * item.quantity
+        total_amount += subtotal
+
+        order_items.append(
+            OrderItems(
+                product_id=product.id,
+                quantity=item.quantity,
+                seller_id=product.seller_id,
+                price_at_purchase=product.price
+            )
+        )
+
+    if total_amount == 0:
+        raise HTTPException(status_code=400, detail="Invalid cart")
+
     
     order = Order(
         user_id=current_user.id,
-        amount=checkout.amount,
+        amount=total_amount,
         shipping_address=checkout.shipping_address,
         status="PLACED",
         currency="INR"
     )
 
+
     db.add(order)
+    db.flush() 
+    
+    for oi in order_items:
+        oi.order_id = order.id
+
+    db.add_all(order_items)
+
     db.commit()
 
     
